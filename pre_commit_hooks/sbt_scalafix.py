26c231dd5118e8841f8aaaabc2590cffe9c82a07
tadej <tadej.ostanek@slicelife.com>
2024-11-19 17:52:19 +0200

    add autoformat to scalafix too

--- pre_commit_hooks/sbt_scalafix.py
+++ pre_commit_hooks/sbt_scalafix.py
@@ -1,15 +1,17 @@
 from pre_commit_hooks.runner import run_sbt_command
 from colorama import init as colorama_init, Fore
 
-TASK_SCALAFIX = 'scalafixAll --check'
+TASK_SCALAFIX = 'scalafixAll' # this one changes the files
+TASK_SCALAFIX_CHECK = 'scalafixAll --check'
 MISSING_PLUGIN_CHECK_STRING = 'Not a valid key: scalafix'
 MISSING_PLUGIN_ERROR_MSG = f'{Fore.RED}ERROR: Scalafix SBT plugin not present! See {Fore.BLUE}https://scalacenter.github.io/scalafix/docs/users/installation.html{Fore.RED} for installation instructions.'
 
 def main(argv=None):
     colorama_init()
 
-    return run_sbt_command(f'; clean ; {TASK_SCALAFIX}', MISSING_PLUGIN_CHECK_STRING, MISSING_PLUGIN_ERROR_MSG)
-
+    check_exit_code = run_sbt_command(f'; clean ; {TASK_SCALAFIX_CHECK}', MISSING_PLUGIN_CHECK_STRING, MISSING_PLUGIN_ERROR_MSG)
+    format_exit_code = run_sbt_command(f'; clean ; {TASK_SCALAFIX}', MISSING_PLUGIN_CHECK_STRING, MISSING_PLUGIN_ERROR_MSG)
+    return check_exit_code + format_exit_code
 
 if __name__ == '__main__':
     exit(main())
