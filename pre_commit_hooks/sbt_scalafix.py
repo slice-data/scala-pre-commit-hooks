5b7c4a9c8d2298dbf7591a5cbd3e75477d1d91a3
tadej <tadej.ostanek@slicelife.com>
2024-11-19 18:11:01 +0200

    fix

--- pre_commit_hooks/sbt_scalafix.py
+++ pre_commit_hooks/sbt_scalafix.py
@@ -5,12 +5,13 @@ TASK_SCALAFIX = 'scalafixAll' # this one changes the files
 TASK_SCALAFIX_CHECK = 'scalafixAll --check'
 MISSING_PLUGIN_CHECK_STRING = 'Not a valid key: scalafix'
 MISSING_PLUGIN_ERROR_MSG = f'{Fore.RED}ERROR: Scalafix SBT plugin not present! See {Fore.BLUE}https://scalacenter.github.io/scalafix/docs/users/installation.html{Fore.RED} for installation instructions.'
-SCALAC_OPTION = '-Ywarn-unused-import'
+SCALAC_OPTIONS = 'set scalacOptions ++= Seq("-Ywarn-unused-import")'
+SCALAFIX_ENABLE = 'scalafixEnable'
 
 def main(argv=None):
     colorama_init()
 
-    check_exit_code = run_sbt_command(f'; clean ; scalafixEnable ; set scalacOptions ++= Seq("{SCALAC_OPTION}") ; {TASK_SCALAFIX_CHECK}', MISSING_PLUGIN_CHECK_STRING, MISSING_PLUGIN_ERROR_MSG)
+    check_exit_code = run_sbt_command(f'; clean ; {SCALAFIX_ENABLE} ; {SCALAC_OPTIONS} ; {TASK_SCALAFIX_CHECK}', MISSING_PLUGIN_CHECK_STRING, MISSING_PLUGIN_ERROR_MSG)
     format_exit_code = run_sbt_command(f'; {TASK_SCALAFIX}', MISSING_PLUGIN_CHECK_STRING, MISSING_PLUGIN_ERROR_MSG)
     return check_exit_code + format_exit_code
 
