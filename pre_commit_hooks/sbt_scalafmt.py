917be180127c3489cf2f34e52da838eed99d3a15
Tadej <tadej.ostanek@slicelife.com>
2024-11-21 13:53:33 +0200

    feat: add scalafix (#1)
    * Setup README for internal use
    
    * fix python issue
    
    * fix new pre-commit version
    
    * remove deprecated push stage semantics  -> pre-push
    
    * consistent hook script naming
    
    * added scalafix hook
    
    * pre-push in readme too
    
    * added PR template
    
    * added scalafix info to readme
    
    * adding check fails the task
    
    * commit stage is renamed too
    
    * Missing entrypoints
    
    * check and then modify
    
    this should fail when formatting is needed, but also apply that formatting
    
    * correct command for check
    
    * no need to run clean for formatting
    
    * Both commands should succeed
    
    * add autoformat to scalafix too
    
    * fix variable name
    
    * set required scalac options for scalafix
    
    * corrected scalacoption
    
    * enable scalafix
    
    * fix
    
    * enable automatically
    
    * run enablement as separate command
    
    * needs to run within the same command - same session
    
    * improve readme
    
    * added command to readme for pre-hooks

--- pre_commit_hooks/sbt_scalafmt.py
+++ pre_commit_hooks/sbt_scalafmt.py
@@ -1,7 +1,8 @@
 from pre_commit_hooks.runner import run_sbt_command
 from colorama import init as colorama_init, Fore
 
-TASK_SCALAFMT = 'scalafmtCheckAll'
+TASK_SCALAFMT = 'scalafmtAll'
+TASK_SCALACHA = 'scalafmtCheckAll'
 MISSING_PLUGIN_CHECK_STRING = 'Not a valid key: scalafmtCheck'
 MISSING_PLUGIN_ERROR_MSG = f'{Fore.RED}ERROR: scalafmt SBT plugin not present! See {Fore.BLUE}https://scalameta.org/scalafmt/docs/installation.html#sbt{Fore.RED} for installation instructions.'
 
@@ -9,8 +10,9 @@ MISSING_PLUGIN_ERROR_MSG = f'{Fore.RED}ERROR: scalafmt SBT plugin not present! S
 def main(argv=None):
     colorama_init()
 
-    return run_sbt_command(f'; clean ; {TASK_SCALAFMT}', MISSING_PLUGIN_CHECK_STRING, MISSING_PLUGIN_ERROR_MSG)
-
+    check_exit_code = run_sbt_command(f'; {TASK_SCALACHA}', MISSING_PLUGIN_CHECK_STRING, MISSING_PLUGIN_ERROR_MSG)
+    format_exit_code = run_sbt_command(f'; {TASK_SCALAFMT}', MISSING_PLUGIN_CHECK_STRING, MISSING_PLUGIN_ERROR_MSG)
+    return check_exit_code + format_exit_code
 
 if __name__ == '__main__':
     exit(main())
