6506165360c0f71c3adab39a52de9aac03b6c527
tadej <tadej.ostanek@slicelife.com>
2024-12-31 12:11:18 +0200

    Run scalafix and scalafmt only for changed files

--- pre_commit_hooks/sbt_scalafix.py
+++ pre_commit_hooks/sbt_scalafix.py
@@ -1,15 +1,30 @@
 from pre_commit_hooks.runner import run_sbt_command
 from colorama import init as colorama_init, Fore
+from typing import Optional
+
+import sys
 
 TASK_SCALAFIX = 'scalafixAll' # this one changes the files
 TASK_SCALAFIX_CHECK = 'scalafixAll --check'
 MISSING_PLUGIN_CHECK_STRING = 'Not a valid key: scalafix'
-MISSING_PLUGIN_ERROR_MSG = f'{Fore.RED}ERROR: Scalafix SBT plugin not present! See {Fore.BLUE}https://scalacenter.github.io/scalafix/docs/users/installation.html{Fore.RED} for installation instructions.'
+MISSING_PLUGIN_ERROR_MSG: str = f'{Fore.RED}ERROR: Scalafix SBT plugin not present! See {Fore.BLUE}https://scalacenter.github.io/scalafix/docs/users/installation.html{Fore.RED} for installation instructions.'
 
-def main(argv=None):
+def main(argv: Optional[list[str]] = None) -> int:
     colorama_init()
-    check_exit_code = run_sbt_command(f'; {TASK_SCALAFIX_CHECK}', MISSING_PLUGIN_CHECK_STRING, MISSING_PLUGIN_ERROR_MSG)
-    format_exit_code = run_sbt_command(f'; {TASK_SCALAFIX}', MISSING_PLUGIN_CHECK_STRING, MISSING_PLUGIN_ERROR_MSG)
+
+    # Get file arguments passed from pre-commit
+    files_arg: list[str] = argv or sys.argv[1:]
+
+    # Only process files with .scala or .sbt extensions
+    scala_files: list[str] = [file for file in files_arg if file.endswith(('.scala', '.sbt'))]
+
+    if not scala_files:
+        print(f"{Fore.YELLOW}No Scala or SBT files to format.")
+        return 0
+    
+    files_cmd_input: str = " ".join(scala_files)
+    check_exit_code: int = run_sbt_command(f'; {TASK_SCALAFIX_CHECK} --files {files_cmd_input}', MISSING_PLUGIN_CHECK_STRING, MISSING_PLUGIN_ERROR_MSG)
+    format_exit_code: int = run_sbt_command(f'; {TASK_SCALAFIX} --files {files_cmd_input}', MISSING_PLUGIN_CHECK_STRING, MISSING_PLUGIN_ERROR_MSG)
     return check_exit_code + format_exit_code
 
 if __name__ == '__main__':
--- pre_commit_hooks/sbt_scalafmt.py
+++ pre_commit_hooks/sbt_scalafmt.py
@@ -1,17 +1,32 @@
 from pre_commit_hooks.runner import run_sbt_command
 from colorama import init as colorama_init, Fore
+from typing import Optional
+
+import sys
 
 TASK_SCALAFMT = 'scalafmtAll'
 TASK_SCALACHA = 'scalafmtCheckAll'
 MISSING_PLUGIN_CHECK_STRING = 'Not a valid key: scalafmtCheck'
-MISSING_PLUGIN_ERROR_MSG = f'{Fore.RED}ERROR: scalafmt SBT plugin not present! See {Fore.BLUE}https://scalameta.org/scalafmt/docs/installation.html#sbt{Fore.RED} for installation instructions.'
+MISSING_PLUGIN_ERROR_MSG: str = f'{Fore.RED}ERROR: scalafmt SBT plugin not present! See {Fore.BLUE}https://scalameta.org/scalafmt/docs/installation.html#sbt{Fore.RED} for installation instructions.'
 
 
-def main(argv=None):
+def main(argv: Optional[list[str]] = None) -> int:
     colorama_init()
 
-    check_exit_code = run_sbt_command(f'; {TASK_SCALACHA}', MISSING_PLUGIN_CHECK_STRING, MISSING_PLUGIN_ERROR_MSG)
-    format_exit_code = run_sbt_command(f'; {TASK_SCALAFMT}', MISSING_PLUGIN_CHECK_STRING, MISSING_PLUGIN_ERROR_MSG)
+    # Get file arguments passed from pre-commit
+    files_arg: list[str] = argv or sys.argv[1:]
+
+    # Only process files with .scala or .sbt extensions
+    scala_files: list[str] = [file for file in files_arg if file.endswith(('.scala', '.sbt'))]
+
+    if not scala_files:
+        print(f"{Fore.YELLOW}No Scala or SBT files to format.")
+        return 0
+    
+    files_cmd_input: str = " ".join(scala_files)
+
+    check_exit_code: int = run_sbt_command(f'; {TASK_SCALACHA} --files {files_cmd_input}', MISSING_PLUGIN_CHECK_STRING, MISSING_PLUGIN_ERROR_MSG)
+    format_exit_code: int = run_sbt_command(f'; {TASK_SCALAFMT} --files {files_cmd_input}', MISSING_PLUGIN_CHECK_STRING, MISSING_PLUGIN_ERROR_MSG)
     return check_exit_code + format_exit_code
 
 if __name__ == '__main__':
